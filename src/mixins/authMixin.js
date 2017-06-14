6262b4d0e6e10261f32cd92ebbb77b90671c5ca8
Mark Zorn <mrzorn@gmail.com>
2017-06-14 15:36:08 -0400

    removed one more

--- src/mixins/authMixin.js
+++ src/mixins/authMixin.js
@@ -13,7 +13,6 @@ module.exports = {
   },
   created(){
     var oauthToken = Vue.cookie.get('shareablee_oauth_token');
-    console.log('oauthToken ' + oauthToken);
 
     if(!oauthToken){
       this.redirectToLogin();
